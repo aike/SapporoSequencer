340d22b421fc6daac7e5a9b2e5abc00b101a74b7
aike <aaiikkee@gmail.com>
2025-11-18 16:43:54 +0900

    debug xy calc

--- mapdemo/server.ps1
+++ mapdemo/server.ps1
@@ -18,7 +18,7 @@ try{
 
   $row = -1
   $col = -1
-  $geodata = "[${row},${col}]"
+  $geodata = "[${col},${row}]"
 
   while ($true){
     $ctx = $listener.GetContext()
@@ -39,59 +39,59 @@ try{
     $content = [byte[]]@()
     switch ($path) {
 
-      'pad/00' { $row = 0; $col = 0; $geodata = "[${row},${col}]" }
-      'pad/01' { $row = 0; $col = 1; $geodata = "[${row},${col}]" }
-      'pad/02' { $row = 0; $col = 2; $geodata = "[${row},${col}]" }
-      'pad/03' { $row = 0; $col = 3; $geodata = "[${row},${col}]" }
-      'pad/04' { $row = 0; $col = 4; $geodata = "[${row},${col}]" }
-      'pad/05' { $row = 0; $col = 5; $geodata = "[${row},${col}]" }
-      'pad/06' { $row = 0; $col = 6; $geodata = "[${row},${col}]" }
-      'pad/07' { $row = 0; $col = 7; $geodata = "[${row},${col}]" }
+      'pad/00' { $row = 0*2; $col = 0; $geodata = "[${col},${row}]" }
+      'pad/01' { $row = 0*2; $col = 1; $geodata = "[${col},${row}]" }
+      'pad/02' { $row = 0*2; $col = 2; $geodata = "[${col},${row}]" }
+      'pad/03' { $row = 0*2; $col = 3; $geodata = "[${col},${row}]" }
+      'pad/04' { $row = 0*2; $col = 4; $geodata = "[${col},${row}]" }
+      'pad/05' { $row = 0*2; $col = 5; $geodata = "[${col},${row}]" }
+      'pad/06' { $row = 0*2; $col = 6; $geodata = "[${col},${row}]" }
+      'pad/07' { $row = 0*2; $col = 7; $geodata = "[${col},${row}]" }
 
-      'pad/10' { $row = 1; $col = 0; $geodata = "[${row},${col}]" }
-      'pad/11' { $row = 1; $col = 1; $geodata = "[${row},${col}]" }
-      'pad/12' { $row = 1; $col = 2; $geodata = "[${row},${col}]" }
-      'pad/13' { $row = 1; $col = 3; $geodata = "[${row},${col}]" }
-      'pad/14' { $row = 1; $col = 4; $geodata = "[${row},${col}]" }
-      'pad/15' { $row = 1; $col = 5; $geodata = "[${row},${col}]" }
-      'pad/16' { $row = 1; $col = 6; $geodata = "[${row},${col}]" }
-      'pad/17' { $row = 1; $col = 7; $geodata = "[${row},${col}]" }  
+      'pad/10' { $row = 1*2; $col = 0; $geodata = "[${col},${row}]" }
+      'pad/11' { $row = 1*2; $col = 1; $geodata = "[${col},${row}]" }
+      'pad/12' { $row = 1*2; $col = 2; $geodata = "[${col},${row}]" }
+      'pad/13' { $row = 1*2; $col = 3; $geodata = "[${col},${row}]" }
+      'pad/14' { $row = 1*2; $col = 4; $geodata = "[${col},${row}]" }
+      'pad/15' { $row = 1*2; $col = 5; $geodata = "[${col},${row}]" }
+      'pad/16' { $row = 1*2; $col = 6; $geodata = "[${col},${row}]" }
+      'pad/17' { $row = 1*2; $col = 7; $geodata = "[${col},${row}]" }  
 
-      'pad/20' { $row = 2; $col = 0; $geodata = "[${row},${col}]" }
-      'pad/21' { $row = 2; $col = 1; $geodata = "[${row},${col}]" }
-      'pad/22' { $row = 2; $col = 2; $geodata = "[${row},${col}]" }
-      'pad/23' { $row = 2; $col = 3; $geodata = "[${row},${col}]" }
-      'pad/24' { $row = 2; $col = 4; $geodata = "[${row},${col}]" }
-      'pad/25' { $row = 2; $col = 5; $geodata = "[${row},${col}]" }
-      'pad/26' { $row = 2; $col = 6; $geodata = "[${row},${col}]" }
-      'pad/27' { $row = 2; $col = 7; $geodata = "[${row},${col}]" }  
+      'pad/20' { $row = 2*2; $col = 0; $geodata = "[${col},${row}]" }
+      'pad/21' { $row = 2*2; $col = 1; $geodata = "[${col},${row}]" }
+      'pad/22' { $row = 2*2; $col = 2; $geodata = "[${col},${row}]" }
+      'pad/23' { $row = 2*2; $col = 3; $geodata = "[${col},${row}]" }
+      'pad/24' { $row = 2*2; $col = 4; $geodata = "[${col},${row}]" }
+      'pad/25' { $row = 2*2; $col = 5; $geodata = "[${col},${row}]" }
+      'pad/26' { $row = 2*2; $col = 6; $geodata = "[${col},${row}]" }
+      'pad/27' { $row = 2*2; $col = 7; $geodata = "[${col},${row}]" }  
 
-      'pad/30' { $row = 3; $col = 0; $geodata = "[${row},${col}]" }
-      'pad/31' { $row = 3; $col = 1; $geodata = "[${row},${col}]" }
-      'pad/32' { $row = 3; $col = 2; $geodata = "[${row},${col}]" }
-      'pad/33' { $row = 3; $col = 3; $geodata = "[${row},${col}]" }
-      'pad/34' { $row = 3; $col = 4; $geodata = "[${row},${col}]" }
-      'pad/35' { $row = 3; $col = 5; $geodata = "[${row},${col}]" }
-      'pad/36' { $row = 3; $col = 6; $geodata = "[${row},${col}]" }
-      'pad/37' { $row = 3; $col = 7; $geodata = "[${row},${col}]" }  
+      'pad/30' { $row = 3*2; $col = 0; $geodata = "[${col},${row}]" }
+      'pad/31' { $row = 3*2; $col = 1; $geodata = "[${col},${row}]" }
+      'pad/32' { $row = 3*2; $col = 2; $geodata = "[${col},${row}]" }
+      'pad/33' { $row = 3*2; $col = 3; $geodata = "[${col},${row}]" }
+      'pad/34' { $row = 3*2; $col = 4; $geodata = "[${col},${row}]" }
+      'pad/35' { $row = 3*2; $col = 5; $geodata = "[${col},${row}]" }
+      'pad/36' { $row = 3*2; $col = 6; $geodata = "[${col},${row}]" }
+      'pad/37' { $row = 3*2; $col = 7; $geodata = "[${col},${row}]" }  
 
-      'pad/40' { $row = 4; $col = 0; $geodata = "[${row},${col}]" }
-      'pad/41' { $row = 4; $col = 1; $geodata = "[${row},${col}]" }
-      'pad/42' { $row = 4; $col = 2; $geodata = "[${row},${col}]" }
-      'pad/43' { $row = 4; $col = 3; $geodata = "[${row},${col}]" }
-      'pad/44' { $row = 4; $col = 4; $geodata = "[${row},${col}]" }
-      'pad/45' { $row = 4; $col = 5; $geodata = "[${row},${col}]" }
-      'pad/46' { $row = 4; $col = 6; $geodata = "[${row},${col}]" }
-      'pad/47' { $row = 4; $col = 7; $geodata = "[${row},${col}]" }  
+      'pad/40' { $row = 4*2; $col = 0; $geodata = "[${col},${row}]" }
+      'pad/41' { $row = 4*2; $col = 1; $geodata = "[${col},${row}]" }
+      'pad/42' { $row = 4*2; $col = 2; $geodata = "[${col},${row}]" }
+      'pad/43' { $row = 4*2; $col = 3; $geodata = "[${col},${row}]" }
+      'pad/44' { $row = 4*2; $col = 4; $geodata = "[${col},${row}]" }
+      'pad/45' { $row = 4*2; $col = 5; $geodata = "[${col},${row}]" }
+      'pad/46' { $row = 4*2; $col = 6; $geodata = "[${col},${row}]" }
+      'pad/47' { $row = 4*2; $col = 7; $geodata = "[${col},${row}]" }  
 
-      'pad/50' { $row = 5; $col = 0; $geodata = "[${row},${col}]" }
-      'pad/51' { $row = 5; $col = 1; $geodata = "[${row},${col}]" }
-      'pad/52' { $row = 5; $col = 2; $geodata = "[${row},${col}]" }
-      'pad/53' { $row = 5; $col = 3; $geodata = "[${row},${col}]" }
-      'pad/54' { $row = 5; $col = 4; $geodata = "[${row},${col}]" }
-      'pad/55' { $row = 5; $col = 5; $geodata = "[${row},${col}]" }
-      'pad/56' { $row = 5; $col = 6; $geodata = "[${row},${col}]" }
-      'pad/57' { $row = 5; $col = 7; $geodata = "[${row},${col}]" }  
+      'pad/50' { $row = 5*2; $col = 0; $geodata = "[${col},${row}]" }
+      'pad/51' { $row = 5*2; $col = 1; $geodata = "[${col},${row}]" }
+      'pad/52' { $row = 5*2; $col = 2; $geodata = "[${col},${row}]" }
+      'pad/53' { $row = 5*2; $col = 3; $geodata = "[${col},${row}]" }
+      'pad/54' { $row = 5*2; $col = 4; $geodata = "[${col},${row}]" }
+      'pad/55' { $row = 5*2; $col = 5; $geodata = "[${col},${row}]" }
+      'pad/56' { $row = 5*2; $col = 6; $geodata = "[${col},${row}]" }
+      'pad/57' { $row = 5*2; $col = 7; $geodata = "[${col},${row}]" }  
 
       'geodata'{ 
         "get geodata"|oh
@@ -107,7 +107,7 @@ try{
       }
     }
     $path |oh
-    "set data [${row},${col}]"|oh
+    "set data [${col},${row}]"|oh
 
     $res.OutputStream.Write($content, 0, $content.Length)
     $res.Close()
